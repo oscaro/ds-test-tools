91d04bf33e74eee37de3d88b1a2a087f20a76299
Baptiste Fontaine <baptiste.fontaine@oscaro.com>
2020-06-15 14:08:57 +0200

    CI: add auto-deploy

--- project.clj
+++ project.clj
@@ -6,7 +6,14 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [datasplash          "0.6.4"]
                  [com.oscaro/tools-io "0.3.17"]]
-  :deploy-repositories [["releases" {:url "https://clojars.org/repo"}]]
+  :deploy-repositories [["snapshots" {:url "https://repo.clojars.org"
+                                      :username :env/clojars_username
+                                      :password :env/clojars_password
+                                      :sign-releases false}]
+                        ["releases"  {:url "https://repo.clojars.org"
+                                      :username :env/clojars_username
+                                      :password :env/clojars_password
+                                      :sign-releases false}]]
   :aot :all
   :profiles {:dev {:global-vars {*warn-on-reflection* true}
                    :plugins [[lein-codox "0.10.2"]]
