4d49a942e5f301948d68b79a65b4f4f7d2fbd868
iomonad <iomonad@riseup.net>
2022-07-04 21:05:09 +0200

    upgraded dependencies

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [datasplash          "0.6.4"]
-                 [com.oscaro/tools-io "0.3.17"]]
+                 [datasplash          "0.7.10"]
+                 [com.oscaro/tools-io "0.3.26"]]
   :deploy-repositories [["snapshots" {:url "https://repo.clojars.org"
                                       :username :env/clojars_username
                                       :password :env/clojars_password
@@ -17,5 +17,5 @@
   :aot :all
   :profiles {:dev {:global-vars {*warn-on-reflection* true}
                    :plugins [[lein-codox "0.10.2"]]
-                   :dependencies [[org.clojure/tools.namespace "0.2.11"]]
+                   :dependencies [[org.clojure/tools.namespace "1.3.0"]]
                    :source-paths ["dev"]}})
