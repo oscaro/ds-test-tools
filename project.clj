568f71cc6e7846aed6a5020531997348bc3c6f94
iomonad <iomonad@riseup.net>
2024-10-04 14:13:00 +0200

    bump: datasplash 0.7.23

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/oscaro/ds-test-tools"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [datasplash          "0.7.18"]
+  :dependencies [[org.clojure/clojure "1.11.0"]
+                 [datasplash          "0.7.23"]
                  [com.oscaro/tools-io "0.3.26"]]
   :deploy-repositories [["snapshots" {:url "https://repo.clojars.org"
                                       :username :env/clojars_username
@@ -17,5 +17,5 @@
   :aot :all
   :profiles {:dev {:global-vars {*warn-on-reflection* true}
                    :plugins [[lein-codox "0.10.2"]]
-                   :dependencies [[org.clojure/tools.namespace "1.3.0"]]
+                   :dependencies [[org.clojure/tools.namespace "1.5.0"]]
                    :source-paths ["dev"]}})
