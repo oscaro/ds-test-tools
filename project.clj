aa7df0a8d1e77fecda9279bf6cca855d8d8e1bd3
Ed Porras <dev@digressed.net>
2025-06-11 09:24:49 +0200

    build: bump clojure (1.12.1), ds (0.7.30), tio (0.3.41)

--- project.clj
+++ project.clj
@@ -3,9 +3,10 @@
   :url "https://github.com/oscaro/ds-test-tools"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.11.0"]
-                 [datasplash          "0.7.23"]
-                 [com.oscaro/tools-io "0.3.26"]]
+  :dependencies [[org.clojure/clojure "1.12.1"]
+                 [datasplash          "0.7.30"]
+                 [com.oscaro/tools-io "0.3.41"]
+                 [com.github.luben/zstd-jni "1.5.7-3"]]
   :deploy-repositories [["snapshots" {:url "https://repo.clojars.org"
                                       :username :env/clojars_username
                                       :password :env/clojars_password
